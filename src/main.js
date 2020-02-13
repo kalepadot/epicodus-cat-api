532dc8bcb04402619c85ee869ee248b49bd427bf
Student <example@example.com>
2020-02-13 09:33:34 -0800

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 // import { example } from './example';
 import './scss/main.scss';
 import $ from 'jquery';
+import './kitty-kat/.env';
 
 $(document).ready(function() {
   $("#giveKitty").click(function() {
     let request = new XMLHttpRequest;
-    const url = `https://api.thecatapi.com/v1/images/search?appid=71f68b35-41ce-4544-8e35-a7bd2c8808e0`;
+    const url = `https://api.thecatapi.com/v1/images/search?appid=[]`;
     request.onreadystatechange = function() {
       if (this.readyState === 4 && this.status === 200) {
         const response = JSON.parse(this.responseText);
